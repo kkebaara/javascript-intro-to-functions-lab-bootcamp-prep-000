3ef1d683a5866607b1c2ad2c87ded0ad0abc9fb4
k k <kkebaara@yahoo.com>
2021-01-23 06:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function sayHiToGrandma(string){
 
 if (string.toLowerCase === string) {
   return "I can't hear you!"
-} else if (string === string.toUpperCase) {
+}
+
+if (string === string.toUpperCase) {
   return "YES INDEED!"
 } else if (string === "I love you, Grandma."){
   return  'I love you, too.'
