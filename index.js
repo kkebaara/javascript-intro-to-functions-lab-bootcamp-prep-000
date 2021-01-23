11c8363f9ba6efbec99972913b1c7d82044e6824
k k <kkebaara@yahoo.com>
2021-01-23 06:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,15 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string){
-    return "I can't hear you!"
-  } else if (string.toUpperCase === string){
-    return "YES INDEED!"
-  } else {
-    return  'I love you, too.'
-  }
+
+if (string === string.toLowerCase) {
+  return "I can't hear you!"
+} else if (string === string.toUpperCase) {
+  return "YES INDEED!"
+} else {
+  return  'I love you, too.'
+}
+
 }
