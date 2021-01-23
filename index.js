15ae039c8e4ab0e88976dc0839e009b1597b46bc
k k <kkebaara@yahoo.com>
2021-01-23 06:41:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (string === string.toLowerCase) {
   return "I can't hear you!"
 } else if (string === string.toUpperCase) {
   return "YES INDEED!"
-} else {
+} else if (string === "I love you, Grandma."){
   return  'I love you, too.'
 }
 
